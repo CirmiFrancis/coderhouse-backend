9cb599341ada0d79b4bde5ceb359192d6f850516
Francis Cirmi <cirmifrancis@gmail.com>
2024-03-08 17:43:18 -0300

    Desafio2 hecho.

--- desafio2/ProductManager.js
+++ desafio2/ProductManager.js
@@ -1,4 +1,4 @@
-// ====== CÓDIGO ======
+// ================== CÓDIGO ==================
 
 const fs = require("fs"); //cargamos el File System de Node.js
 
@@ -10,7 +10,6 @@ class ProductManager {
     }
 
     addProduct(title, description, price, thumbnail, code, stock) {
-
         if(!title || !description || !price || !thumbnail || !code || !stock) {
             console.log("Por favor, completa todos los campos.");
             return;
@@ -32,20 +31,23 @@ class ProductManager {
         }
 
         this.products.push(newProduct);
-
-        const saveJson = async () => { //guardamos los productos en un JSON
-            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
-        } 
-        saveJson();
+        this.saveProducts();
     }
 
-    getProducts(){ //obtenemos los productos
+    saveProducts() { //guardar productos en JSON
+        fs.writeFile(this.path, JSON.stringify(this.products, null, 2), err => {
+            if (err) {
+                console.error('Error, no se pudo guardar los productos:', err);
+            }
+        });
+    }
+
+    getProducts() { //obtenemos los productos
         return this.products;
     }
 
-    getProductById(id){
-        //const producto = this.products.find(producto => producto.id === id);
-        const producto = this.products.find(producto => producto.id === id); //teniendo el array, buscamos un producto por su id
+    getProductById(id) { //teniendo el array, buscamos un producto por su id
+        const producto = this.products.find(product => product.id === id); 
 
         if (producto) {
             return (`Producto con el ID ${producto.id} encontrado:`, producto);
@@ -55,29 +57,20 @@ class ProductManager {
         }
     }
 
-    updateProduct(id, updatedProduct){ //método creado para actualizar el producto por id
+    updateProduct(id, updatedFields) { //actualiza la información del producto que coincida con el id
         const index = this.products.findIndex(product => product.id === id);
         if (index !== -1) {
-            //this.products[index] = { ...this.products[index], ...updatedProduct };
-            this.products[index] = {...this.products[index], ...updatedProduct};
-
-            const saveJson = async () => { //guardamos el nuevo array en el JSON
-                await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
-            } 
-            saveJson();
+            this.products[index] = { ...this.products[index], ...updatedFields };
+            this.saveProducts();
         } else {
-            return ("Error, producto no encontrado para actualizar.");
+            console.log("Error, producto no encontrado para actualizar.");
         }
     }
 
-    deleteProduct(id){ //método creado para eliminar un producto por id
+    deleteProduct(id){ //eliminamos el producto con el id que coincida y actualizamos el array
         if (this.products.some(product => product.id === id)) {
             this.products = this.products.filter(product => product.id !== id);
-
-            const saveJson = async () => { //guardamos el nuevo array en el JSON
-                await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
-            } 
-            saveJson();
+            this.saveProducts();
         }
         else {
             console.log("Error, producto no encontrado para eliminar.");
@@ -85,7 +78,7 @@ class ProductManager {
     }
 }
 
-// ====== TESTEO ======
+// ================== TESTEO ==================
 
 // cd desafio2
 // node ProductManager.js
@@ -97,9 +90,10 @@ const manager = new ProductManager("./products.json");
 console.log(manager.getProducts());
 
 // 3) Se llama al método 'addProduct' para agregar productos al array. El ID debe autoincrementarse y no repetirse
-manager.addProduct("producto prueba", "este es un producto prueba", 200, "sin imagen", "abc123", 25);
-manager.addProduct("fideos", "los mas ricos", 200, "sin imagen", "abc124", 50);
-manager.addProduct("arroz", "el que no se pasa", 300, "sin imagen", "abc124", 150); // Tira error por tener el mismo 'code'
+manager.addProduct("Producto Prueba", "Este es un producto prueba.", 100, "Sin imagen", "code123",  50);
+manager.addProduct("Hamburguesa",     "Completa y muy rica.",        200, "Sin imagen", "code456", 100);
+manager.addProduct("Pancho",          "Con papitas y aderezos.",     50,  "Sin imagen", "code789",  25);
+manager.addProduct("Producto Error",  "Este es un producto error.",  100, "Sin imagen", "code123",  50); // Tira error por tener el mismo 'code'
 
 // 4) Se llama al método 'getProducts', el cual devuelve el array actualizado
 console.log(manager.getProducts());
@@ -111,11 +105,11 @@ console.log(manager.getProductById(4)); // Tira error por no encontrar un produc
 // 6) Se llama al método 'updateProduct', el cual nos permite cambiar/actualizar la información de un producto
 manager.updateProduct(2, {
     title: "Producto 2 Actualizado",
-    description: "Descripción",
+    description: "Descripción del producto.",
     price: 100,
-    thumbnail: "sin img",
-    code: "abc133",
-    stock: 20
+    thumbnail: "Con imagen",
+    code: "code777",
+    stock: 50
 });
 console.log(manager.getProducts());
 
