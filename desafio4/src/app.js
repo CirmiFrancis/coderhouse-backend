55b1fbef5c7bb8f475017f46eefedcbf3bc69b95
Francis Cirmi <cirmifrancis@gmail.com>
2024-03-27 16:38:20 -0300

    Clase 10 & Clase 11. Websocket, Socket.io & Glitch.

---
+++ desafio4/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express(); 
+const PUERTO = 8080;
+
+import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/api", productsRouter);
+app.use("/api", cartsRouter);
+
+app.get("/", (req, res) => {
+    res.send("Funciona");
+})
+
+app.listen(PUERTO, () => {
+    console.log(`Escuchando en el puerto: ${PUERTO}`);
+})
---
+++ desafio4/src/routes/products.router.js
@@ -0,0 +1,95 @@
+import express from "express";
+const router = express.Router();
+
+import ProductManager from "../controllers/ProductManager.js";
+const productManager = new ProductManager("./src/models/products.json");
+
+// Obtengo todos los productos del JSON
+router.get("/products", async (req, res) => {
+    try {
+        const limit = req.query.limit;
+        const products = await productManager.getProducts();
+
+        if (limit) {
+            res.json(products.slice(0, limit));
+        } 
+        else {
+            res.json(products);
+        }
+    } 
+    catch (error) {
+        console.error("Error al obtener productos", error);
+        res.status(500).json({
+            error: "Error interno del servidor"
+        });
+    }
+})
+
+// Obtener un producto por ID
+router.get("/products/:pid", async (req, res) => {
+    const id = req.params.pid;
+
+    try {
+        const product = await productManager.getProductById(parseInt(id));
+
+        if (!product) {
+            return res.json({
+                error: "Producto no encontrado"
+            });
+        }
+
+        res.json(product);
+    } 
+    catch (error) {
+        console.error("Error al obtener producto", error);
+        res.status(500).json({
+            error: "Error interno del servidor"
+        });
+    }
+})
+
+// Agregar un nuevo producto
+router.post("/products", async (req, res) => {
+    const newProduct = req.body; 
+
+    try {
+        await productManager.addProduct(newProduct);
+        res.status(201).json({message: "Producto agregado exitosamente"});
+    } 
+    catch (error) {
+        res.status(500).json({error: "Error interno del servidor"});
+    }
+})
+
+// Actualizar por ID
+router.put("/products/:pid", async (req, res) => {
+    const id = req.params.pid;
+    const updatedProduct = req.body; 
+
+    try {
+        await productManager.updateProduct(parseInt(id), updatedProduct);
+        res.json({
+            message: "Producto actualizado correctamente"
+        });
+    } 
+    catch (error) {
+        res.status(500).json({error: "Error interno del servidor"});
+    }
+})
+
+// Eliminar producto
+router.delete("/products/:pid", async (req, res) => {
+    const id = req.params.pid; 
+
+    try {
+        await productManager.deleteProduct(parseInt(id));
+        res.json({
+            message: "Producto eliminado exitosamente"
+        });
+    } 
+    catch (error) {
+        res.status(500).json({error: "Error interno del servidor"});
+    }
+})
+
+export default router;
