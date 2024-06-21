d832b8ab15ed89ceefd2768f5e157b4855257a6d
Francis Cirmi <cirmifrancis@gmail.com>
2024-06-21 01:46:57 -0300

    pre-entrega 3

---
+++ pre-entrega-3/src/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://franciscirmi:coderhouse@codercluster.u7pr2vc.mongodb.net/ProyectoFinal?retryWrites=true&w=majority&appName=CoderCluster")
+    .then(() => console.log("Conexión exitosa"))
+    .catch(() => console.log("Hay un error"))
+    
--- pre-entrega3/src/utils/reusables.js
+++
@@ -1,8 +0,0 @@
-// BEFORE:  res.status(500).json("Error al obtener los productos.");
-// AFTER:   respon(res, 500, "Error al obtener los productos.")
-
-const respon = (res, status, message) => {
-    res.status(status).json({message});
-}
-
-export default respon;
