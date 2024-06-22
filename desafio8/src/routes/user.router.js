e4778cdc1d1278657ab121a510883530c1538ef1
Francis Cirmi <cirmifrancis@gmail.com>
2024-06-22 03:25:12 -0300

    Desafio 8 hecho. Mock con fakerjs y manejador de errores.

---
+++ desafio8/src/routes/user.router.js
@@ -0,0 +1,14 @@
+import express from "express";
+import passport from "passport";
+import UserController from "../controllers/user.controller.js";
+
+const router = express.Router();
+const userController = new UserController();
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get("/profile", passport.authenticate("jwt", { session: false }), userController.profile);
+router.post("/logout", userController.logout.bind(userController));
+router.get("/admin", passport.authenticate("jwt", { session: false }), userController.admin);
+
+export default router;
---
+++ desafio8/src/sockets/socketmanager.js
@@ -0,0 +1,42 @@
+import { Server } from "socket.io";
+import ProductRepository from "../repositories/product.repository.js";
+import MessageModel from "../models/message.model.js";
+
+const productRepository = new ProductRepository();
+
+class SocketManager {
+    constructor(httpServer) {
+        this.io = new Server(httpServer);
+        this.initSocketEvents();
+    }
+
+    async initSocketEvents() {
+        this.io.on("connection", async (socket) => {
+            console.log("Un cliente se conectó al chat comunitario.");
+            
+            socket.emit("productos", await productRepository.obtenerProductos() );
+
+            socket.on("eliminarProducto", async (id) => {
+                await productRepository.eliminarProducto(id);
+                this.emitUpdatedProducts(socket);
+            });
+
+            socket.on("agregarProducto", async (producto) => {
+                await productRepository.agregarProducto(producto);
+                this.emitUpdatedProducts(socket);
+            });
+
+            socket.on("message", async (data) => {
+                await MessageModel.create(data);
+                const messages = await MessageModel.find();
+                socket.emit("message", messages);
+            });
+        });
+    }
+
+    async emitUpdatedProducts(socket) {
+        socket.emit("productos", await productRepository.obtenerProductos());
+    }
+}
+
+export default SocketManager;
