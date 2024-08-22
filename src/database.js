04d2ef045c25358f845fad3690628b2e577ca72b
Francis Cirmi <cirmifrancis@gmail.com>
2024-08-22 17:40:46 -0300

    parametros de mongodb eliminados

--- src/database.js
+++ src/database.js
@@ -5,6 +5,6 @@ dotenv.config();
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URL)
     .then(() => console.info("Conectado a la base de datos de MongoDB."))
     .catch((error) => console.error("Hay un error en la conexión.", error))
